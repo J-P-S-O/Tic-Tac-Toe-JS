a3f025d1615c6332d7608cde0ce495ea31127d2c
JPSO <66487668+J-P-S-O@users.noreply.github.com>
2020-12-06 16:06:43 -0300

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,15 @@
-let prompt = require("prompt")
+let prompt = require("prompt-sync")()
 let board = [['','',''],['','',''],['','','']];
 let net = require("net")
-let socket = require('socket')
+
 let me = undefined
 let adv = undefined
 const { EventEmitter } = require("events");
 
+
 const firstEmitter = new EventEmitter();
 let server = net.createServer((c) => {
+    console.log("Connected")
     c.on('end', () => {
       console.log('client disconnected');
     });
@@ -32,22 +34,30 @@ client.on('data', function(data) {
 });
 
 function check() {
-    for (i of board) {
-        if (i[0] == i[1] && i[2] == i[1]&&board) {
+    for (i in board) {
+        console.log("board:"+board)
+        console.log("i"+i)
+        console.log(board[i])
+        if (i[0] == i[1] && i[2] == i[1]&&(i[0]!= undefined)||i[0]!='') {
+            console.log("end first")
             end();
+
         }
+    }
         let counter = 0;
         while (counter<3){
-            if (board[0][counter]==board[1][counter]&&board[0][counter]==[board[2][counter]]&&board){
+            if (board[0][counter]==board[1][counter]&&board[0][counter]==[board[2][counter]]&&(board[0][counter]!=undefined)||board[0][counter]!=""){
                 end();
         }
         counter++;
     }
-}
-if ((board[1][1]==board[2][2]&&board[2][2]==board[3][3])||(board[3][1]==board[2][2]&&board[2][2]==board[1][3])&&board){
+    if ((board[0][0]==board[2][2]&&board[2][2]==board[1][1]&& board[1][1]!= undefined)||(board[2][0]==board[1][1]&&board[1][1]==board[0][2])&& board[1][1]!= undefined){
     end();
 }
 }
+
+
+/*
 function changeboard(x,y){
     if (board[x][y]!=''){
         console.log("Err.UsedOrNotOption")
@@ -56,22 +66,20 @@ function changeboard(x,y){
     }
     printBoard()
 }
+*/
 function printBoard(){
-    console.log("BOARD:")
-    for( i in board){
-    console.log(i[0],i[1],i[2])
-    console.log("-----------")
-    }
+    console.log(board)
 }
 
 function end(){
     console.log("The End CheCk ThE bOaRd")
-    process.kill()
+    process.exit()
 }
 function start(){
+    console.log(board)
     console.log("Whats is the peer?None To wait")
-    let peer = prompt.get()
-    if (peer = "None"){
+    let peer = prompt()
+    if (peer == "None"){
         me = "X"
         server.listen(8090)
         console.log("Listening at 8090")
@@ -81,19 +89,20 @@ function start(){
         adv = "X"
         client.connect(8090, peer, function() {
             ErrCode = 0;
+            console.log("connected")
         });
         
-        
+        PersonInput()
     }
 }
 function PersonInput(){
     console.log("Select Line, 0-2")
-    nput = prompt.get()
+    nput = prompt()
     console.log("Select col, 0-2")
-    nput2 = prompt.get()
-    board[nput][nput2] = "X"
-
-    firstEmitter.emit("send",{content: outerinput})
+    nput2 = prompt()
+    board[nput][nput2] = me
+printBoard()
+    firstEmitter.emit("send",{content: nput+nput2})
     OutInput()
     check()
 }
@@ -106,6 +115,7 @@ async function OutInput(){
         }
         outerinput = data.split("")
         board[int(outerinput[0])][int(outerinput[1])] = adv
+        printBoard()
 PersonInput()
 
     });
